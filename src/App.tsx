66d508b22883bfa85db0c081df756059731e200c
rldnd <gi43890@naver.com>
2021-03-03 14:15:29 +0900

    add: eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const App = (): JSX.Element => {
-  return <AppWrapper>되라 얍!</AppWrapper>;
+    return <>hi</>;
 };
-
-const AppWrapper = styled.div`
-  width: 100%;
-  min-height: 100vh;
-
-  background-color: #eee;
-`;
 
 export default App;
